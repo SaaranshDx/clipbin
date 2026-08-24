3257107dc5ca706710c511eb26b76aef1f95a96a
SaaranshDx <saaransh762@gmail.com>
2026-08-24 23:15:28 +0530

    feat(back): added metadata for storing when the paste was created and how long it should persist

--- src/main.py
+++ src/main.py
@@ -1,7 +1,20 @@
+import time
+import json
 PASTES_PATH = "../db/"
 PASTES_META_PATH = f"{PASTES_PATH}/meta"
 
 
+def write_metadata(Id, duration):
+    meta = {
+        "id": Id,
+#duration is in hours ofc
+        "duration": duration,
+        "created_at": time.time()
+    }
+    meta_str = json.dumps(meta, indent=4)
+    with open(f"{PASTES_META_PATH}/id.json", "x") as f:
+        f.write(meta_str)
+        
 def create_paste(id, data):
     try:
         with open(f"{PASTES_PATH}/{id}.txt", "w", encoding="utf-8") as f:
