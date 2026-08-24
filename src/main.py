e970e9efdebbf866522ea04e85667eed2a1bd655
SaaranshDx <saaransh762@gmail.com>
2026-08-24 22:57:59 +0530

    feat(back): added paste read function

--- src/main.py
+++ src/main.py
@@ -4,8 +4,15 @@ PASTES_META_PATH = f"{PASTES_PATH}/meta"
 
 def create_paste(id, data):
     try:
-        with open(f"{PASTES_PATH}/{id}", "w", encoding="utf-8") as f:
+        with open(f"{PASTES_PATH}/{id}.txt", "w", encoding="utf-8") as f:
             f.write(data)
         return True
     except Exception as e:
         return False 
+    
+def read_paste(id):
+    try:
+        data = open(f"{PASTES_PATH}/{id}.txt") 
+        return data
+    except Exception as e:
+        return False   
