a5fc31b7ad0e87c447c08193900c359b39064e8f
SaaranshDx <saaransh762@gmail.com>
2026-08-27 22:33:09 +0530

    feat(cli): added argument parsing

--- cli/main.py
+++ cli/main.py
@@ -3,5 +3,16 @@ import argparse
 import requests
 from getpass import getpass
 
+API_URL = "https://api.mizucode.qzz.io"
+BASE_URL = "https://clipbin.github.io"
+
 def main():
-    print("Hello world!")
+    parser = argparse.ArgumentParser(
+        prog="clipbin",
+        description="share text like never befour"
+    )    
+    parser.add_argument("--encrypt,", action="store_true")
+    parser.add_argument("--duration,", type=int, default=168)
+
+    args = parser.parse_args()
+        
